f967130240ab6687d0217dd7d4b1a2115d37d3a8
bratan <maalaoui.ahmed.am75@gmail.com>
2026-01-20 18:09:38 +0100

    initial commid : add backend + frontend projects

---
+++ frontend/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.test.frontend {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.net.http;
+
+    requires com.google.gson;
+    requires atlantafx.base;
+
+    exports com.testing.frontend;
+}
