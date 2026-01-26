1af3ca46e191fcce044633ea0d37e008acf089fd
bratan <maalaoui.ahmed.am75@gmail.com>
2026-01-26 12:38:41 +0100

    added check email page that shows after sending email for changing password more will be added soon

--- backend/src/Controller/AuthPageController.php
+++ backend/src/Controller/AuthPageController.php
@@ -6,35 +6,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class AuthPageController extends AbstractController
 {
-#[Route('/', name: 'home')]
-public function index(): Response
-{
-    return $this->render('pages/home.html.twig');
-    
-}
-#[Route('/login', name: 'login')]
-public function login(): Response
-{
-    return $this->render('pages/login.html.twig');
-}
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('pages/home.html.twig');
+        
+    }
+    #[Route('/login', name: 'login')]
+    public function login(): Response
+    {
+        return $this->render('pages/login.html.twig');
+    }
 
-#[Route('/register', name:'register')]
-public function register(): Response
-{
-    return $this->render('pages/signup.html.twig');
-}
-#[Route('/forgot-password', name:'forgot_password')]
-public function forgotPassword(): Response
-{
-    return $this->render('pages/forgotPassword.html.twig');
-}
-#[Route('/verify-email', name:'verify-email')]
-public function verifyEmail(): Response
-{
-    return $this->render('pages/verifyEmail.html.twig', [
-        'email' => 'user@example.com' // Placeholder until integrated with previous window
-    ]);
-}
+    #[Route('/register', name:'register')]
+    public function register(): Response
+    {
+        return $this->render('pages/signup.html.twig');
+    }
+    #[Route('/forgot-password', name:'forgot_password', methods: ['GET', 'POST'])]
+    public function forgotPassword(Request $request): Response
+    {
+        if ($request->isMethod('POST')) {
+            // TODO: Process email sending logic here
+            
+            return $this->redirectToRoute('check_email');
+        }
+
+        return $this->render('pages/forgotPassword.html.twig');
+    }
+    #[Route('/verify-email', name:'verify-email')]
+    public function verifyEmail(): Response
+    {
+        return $this->render('pages/verifyEmail.html.twig', [
+            'email' => 'user@example.com' // Placeholder until integrated with previous window
+        ]);
+    }
+
+    #[Route('/check-email', name: 'check_email')]
+    public function checkEmail(): Response
+    {
+        return $this->render('pages/checkEmail.html.twig');
+    }
 }   
